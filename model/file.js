21849bf855856507af62e0af80f2158767af3e01
alpha <123@qq.com>
2020-07-22 19:59:40 +0800

    photo

---
+++ model/file.js
@@ -0,0 +1,15 @@
+// 用于直接操作相册和图片的模块
+const fs=require("fs");
+
+/**
+* @method 读取某个文件夹的内容
+* @param { String } dirName 被读取的文件夹名称或路径
+* @param { Function } cb 回调函数
+*/
+exports.getDirs=function(dirName,cb){
+    fs.readdir(dirName,function(err,files){
+        cb(err,files);
+    });
+    
+}
+
